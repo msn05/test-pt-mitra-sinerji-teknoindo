a9e3c996cf7a3de3173d54c80f8bde466c33d316
msn05 <m.satrion05091997@gmail.com>
2022-08-25 17:43:06 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        return \App\Models\User::create([
+            'name' => 'muhammad satrion',
+            'email' => 'test@gmail.com',
+            'email_verified_at' => '2022-08-22 17:00:00',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => null,
+        ]);
     }
 }
